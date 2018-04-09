654278274ffe5f4117b4ea81c752275340284da1
Xue Snow <snow.xue@fil.com>
2018-04-09 17:13:50 +0800

    add htmlwebpckaplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[hash].js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: "./index.html"
+      })
+  ]
 };
