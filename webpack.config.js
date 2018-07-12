1c81f31967b669a70ecfa83422913a943dbdfb70
Xue Snow <snow.xue@fil.com>
2018-07-12 10:07:07 +0800

    add babel-loader and bable-preset to build it succeed.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack'); //to access built-in plugins
 
 module.exports = {
   mode: 'development',
@@ -8,9 +9,23 @@ module.exports = {
     filename: 'bundle.[hash].js',
     path: path.resolve(__dirname, 'dist')
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-        template: "./index.html"
-      })
-  ]
+  devtool: 'source-map',
+  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules:[
+      {
+        test: /\.scss$/,
+        use: [
+          {loader: "style-loader"},
+          {loader: "css-loader"},
+          {loader: "sass-loader"}
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader'
+      },
+    ]
+  }
 };
