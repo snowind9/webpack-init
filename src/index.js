144a317fde179bb0311d990fb88a639d18e316c7
Xue Snow <snow.xue@fil.com>
2018-07-13 10:26:28 +0800

    add react preset for trans react

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {getInitialNotes} from './store/actions';
+import configureStore from './store/store';
+
+const store = configureStore();
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
